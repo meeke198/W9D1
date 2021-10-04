4c171fd00881eedc4e05871d04aed6417e77bf49
gtsimis <gt2186@gmail.com>
2021-10-04 11:55:14 -0400

    Added index.html, canvas.js and some webpack stuff

---
+++ dist/canvas.js
@@ -0,0 +1,8 @@
+document.getEventListener('DOMContentLoaded', function () {
+    // const canvas = document.getElementById('canvas');
+    canvas.width = 500;
+    canvas.height = 500;
+    // const ctx = canvas.getContext('2d');
+    // ctx.fillStyle = 'pink';
+    // ctx.fillRect(0, 0, 500, 500);
+}
